aa813f9b189679df8058e35512416fe1e48928c5
Andrej Griniuk <andrej.griniuk@gmail.com>
2017-10-28 21:09:53 +0300

    migration

---
+++ config/Migrations/20171028180621_SetSupplierIdentifiers.php
@@ -0,0 +1,36 @@
+<?php
+use Migrations\AbstractMigration;
+
+class SetSupplierIdentifiers extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $Suppliers = \Cake\ORM\TableRegistry::get('Suppliers');
+        /** @var \App\Model\Entity\Supplier[] $suppliers */
+        $suppliers = $Suppliers->find()->all();
+        foreach ($suppliers as $supplier) {
+            if ($supplier->name == 'AGL') {
+                $supplier->slug = 'Alg';
+                $supplier->identifier = 'AGL electricity';
+            } elseif ($supplier->name == 'Sydney Water') {
+                $supplier->slug = 'SydneyWater';
+                $supplier->identifier = 'Website: sydneywater.com.au';
+            } elseif ($supplier->name == 'RED energy') {
+                $supplier->slug = 'RedEnergy';
+                $supplier->identifier = 'redenergy.com.au';
+            } elseif ($supplier->name == 'The Knight Alliance') {
+                $supplier->slug = 'TheKnightAlliance';
+                $supplier->identifier = 'theknight@theknight.com.au';
+            }
+
+            $Suppliers->saveOrFail($supplier);
+        }
+    }
+}
